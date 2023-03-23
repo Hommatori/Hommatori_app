ea57d2b6dcc5b0583dc1c15904843e436b584c90
Aki Kuivas <a-p.kvs@pm.me>
2023-03-23 19:13:14 +0200

    hakukenttä jä testi listaus tehty

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator 
         initialRouteName='MainPage'
-        screenOptions={{headerShown: false}}> 
+        screenOptions={{headerShown: true}}> 
           <Stack.Screen 
             name="MainPage" 
             component={MainPage} 

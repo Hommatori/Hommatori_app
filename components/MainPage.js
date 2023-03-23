77871310a0701d9c7be3a16da2cfcdfba273d640
Aki Kuivas <a-p.kvs@pm.me>
2023-03-23 20:06:33 +0200

    tehty valikko modalilla

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect,useLayoutEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, View, ScrollView, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, ScrollView, Image, Modal, Pressable } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import Styles from '../Styles';
@@ -8,6 +8,7 @@ import Styles from '../Styles';
 export default function MainPage({navigation}) {
 
   const [testData, setTestData] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   // tässä luodaan testitaulukko
   useEffect(() => {
@@ -18,10 +19,12 @@ export default function MainPage({navigation}) {
     setTestData(testArray);
   },[]) 
 
+  // yläPalkki
   useLayoutEffect( () => {
     navigation.setOptions({
         headerStyle: {
             //backroundColor: '#f0f0f0'
+            //fontSize: 30,
         },
         headerRight: () => (
             <Feather
@@ -29,18 +32,48 @@ export default function MainPage({navigation}) {
                 name="menu"
                 size={24}
                 color="black"
-                //onPress={ () => navigation.navigate('Todo')}
+                onPress={ () => setModalVisible(true)}
             />  
             
         ),  
     }) 
 }, [])
 
+//tämä funktio sulkee modalin
+function closeModal() {
+  setModalVisible(false);
+}
+
   return (
 
+    
     <View style={Styles.container}>
+      <Modal 
+      visible={modalVisible}
+      onRequestClose ={closeModal}
+      >
+        <View style={Styles.modal}>
+          <Text> Mitä haluaisit tehdä?</Text>
+          <Pressable onPress={() => {
+            setModalVisible(false);
+          }}>
+            <Text style={Styles.modalItem}>Ilmoita</Text>
+            </Pressable>
+            <Pressable onPress={() => {
+            setModalVisible(false);
+          }}>
+            <Text style={Styles.modalItem}>Asetukset</Text>
+            </Pressable>
+            <Pressable onPress={() => {
+            setModalVisible(false);
+          }}>
+            <Text style={Styles.modalItem}>Peruuta</Text>
+            </Pressable>
+        </View>
+      </Modal>
+
       <View style={Styles.heading}>
-        <Text style={Styles.font}>Tervetuloa hommatorille</Text>
+        <Text style={Styles.font}>Onko remontti tekemättä?</Text>
         <View style={Styles.borders}>
         <TextInput 
             //style={Styles.searchBar}
